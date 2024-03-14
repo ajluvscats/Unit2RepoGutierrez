0de43ed2d21fc3f7856bea103c9cee9096666cc7
ajluvscats <aj_349@icloud.com>
2024-03-14 15:25:55 -0700

    dfdfdfdf

--- Unit2ProjectGutierrez/Assets/Scripts/PlayerController.cs
+++ Unit2ProjectGutierrez/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 10;
 
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,12 +22,19 @@ public class PlayerController : MonoBehaviour
         // Keep the player in bounds
         if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
         if ( transform.position.x > xRange)
         {
-            transform.position = new Vector3(xRange, transform.position.y, transform psotion.z);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
         
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
